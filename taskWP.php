ba1ccf3204b1cf4ffaf4100e3760be1d83d31be6
Troels Abrahamsen <troels@blck.dk>
2014-09-27 22:59:33 +0200

    Tasks can now be attached to a hook with the add_task method.

--- taskWP.php
+++ taskWP.php
@@ -20,8 +20,19 @@ class taskWP {
 
 	}
 
-	function add_task($name, $handler){
-		$this->tasks[$name] = $handler;
+	function add_task($name, $handler, $hook = false, $priority = false, $arguments = false){
+
+		$this->tasks[$name] = array('handler' => $handler);
+
+		if($hook)
+			$this->tasks[$name]['hook'] = $hook;
+
+		if($priority)
+			$this->tasks[$name]['priority'] = $priority;
+
+		if($arguments)
+			$this->tasks[$name]['arguments'] = $arguments;
+
 	}
 
 	function complete_task($name){
@@ -49,23 +60,38 @@ class taskWP {
 	    	return false;
 
 	    /* Loop through each available task */
-      	foreach($this->tasks as $name => $handler){
+      	foreach($this->tasks as $name => $task){
 
       		if(in_array($name, $this->get_completed_tasks()))
       			continue; // This task is completed so we continue the loop
 
-			if(is_callable($handler)){
-				add_action('taskWP/run_task', $handler); // Handler is callable so we hook it to the taskWP/run_task
-				do_action('taskWP/run_task'); // Run the action to execute the handler callback
-				remove_action('taskWP/run_task', $handler); // Remove the handler again
+			if(is_callable($task['handler'])){
+
+				if($task['hook']){
+
+					if(!$prio = $task['priority'])
+						$prio = 10;
+
+					if(!$args = $task['arguments'])
+						$args = 1;
+
+					add_action($task['hook'], $task['handler'], $prio, $args);
+				
+				}
+				else {
+
+					add_action('taskWP/run_task', $task['handler']); // Handler is callable so we hook it to the taskWP/run_task
+					do_action('taskWP/run_task'); // Run the action to execute the handler callback
+					remove_action('taskWP/run_task', $task['handler']); // Remove the handler again
+
+				}
+
 			}
 			else{
-				if(file_exists($handler))
-					include_once $handler; // Handler is a file. Include it.
-
+				if(file_exists($task['handler']))
+					include_once $task['handler']; // Handler is a file. Include it.
 			}
 
-      		$did_tasks = true;
 			$this->complete_task($name);
 
       	}
